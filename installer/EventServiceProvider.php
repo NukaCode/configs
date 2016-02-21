5254f93cb2cef755e5b69c2cd3539110b235534b
Travis Blasingame <stygian.warlock.v2@gmail.com>
2016-02-21 14:03:55 -0600

    Create EventServiceProvider.php

---
+++ installer/EventServiceProvider.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Contracts\Events\Dispatcher as DispatcherContract;
+use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
+
+class EventServiceProvider extends ServiceProvider
+{
+    /**
+     * The event listener mappings for the application.
+     *
+     * @var array
+     */
+    protected $listen = [
+        NukaCode\Users\Events\UserLoggedIn::class => [
+        ],
+        NukaCode\Users\Events\UserRegistered::class => [
+        ],
+    ];
+
+    /**
+     * Register any other events for your application.
+     *
+     * @param  \Illuminate\Contracts\Events\Dispatcher  $events
+     * @return void
+     */
+    public function boot(DispatcherContract $events)
+    {
+        parent::boot($events);
+
+        //
+    }
+}
